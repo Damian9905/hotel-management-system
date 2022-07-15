b6578e6dc623798c2187a82f6feef22a926c451d
Damian Tobiczyk <damian.tobiczyk@gmail.com>
2022-07-15 22:19:39 +0200

    HMS-005 add changing room prices in admin panel

--- hotel-backend/src/main/java/pl/hotel/tobiczyk/controller/AdminPanelController.java
+++ hotel-backend/src/main/java/pl/hotel/tobiczyk/controller/AdminPanelController.java
@@ -2,14 +2,16 @@ package pl.hotel.tobiczyk.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import pl.hotel.tobiczyk.domain.dto.ChangePriceDto;
 import pl.hotel.tobiczyk.domain.dto.RoomDto;
-import pl.hotel.tobiczyk.domain.model.Room;
+import pl.hotel.tobiczyk.domain.model.RoomType;
 import pl.hotel.tobiczyk.repository.RoomTypeRepository;
 import pl.hotel.tobiczyk.service.RoomService;
 
+import javax.transaction.Transactional;
 import javax.validation.Valid;
-import java.net.URI;
 
 @Controller
 @RequestMapping(path="/panel/admin")
@@ -35,11 +37,30 @@ public class AdminPanelController {
     }
 
     @PostMapping("/addRoom")
-    public String addRoom(@ModelAttribute @Valid RoomDto roomDto, Model model) {
-        Room created = roomService.createNewRoom(roomDto);
-        URI location = URI.create(String.format("/rooms/%s", created.getId()));
-        String result = "Dodano nowy pokój";
-        model.addAttribute("result", result);
+    public String addRoom(@ModelAttribute @Valid RoomDto roomDto, BindingResult result, Model model) {
+        if(result.hasErrors()) {
+            model.addAttribute("errors", result.getAllErrors());
+            return addRoomForm(model);
+        }
+        roomService.createNewRoom(roomDto);
         return "addRoom";
+    }
+
+
+    @GetMapping("/changeRoomPrice")
+    public String changeRoomPriceForm(Model model) {
+        model.addAttribute("changePriceDto", new ChangePriceDto());
+        model.addAttribute("roomTypes", roomTypeRepository.findAll());
+        return "changeRoomPrice";
+    }
+
+    @PostMapping("/changeRoomPrice")
+    public String changeRoomPrice(@ModelAttribute @Valid ChangePriceDto changePriceDto, BindingResult result, Model model) {
+        if(result.hasErrors()) {
+            model.addAttribute("errors", result.getAllErrors());
+            return changeRoomPriceForm(model);
+        }
+        roomService.updateRoomTypePrice(changePriceDto);
+        return "changeRoomPrice";
     }
 }
